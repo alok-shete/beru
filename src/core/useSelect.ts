4fde9d53bf87737e3b8453041970c6750c33678d
Alok Shete <alok.shete@vendekin.com>
2025-04-13 16:31:37 +0530

    feat(useSelect): add JSDoc comments for improved documentation of the custom hook
    - Enhanced `useSelect` hook with detailed JSDoc comments to clarify its purpose and usage.
    - Documented type parameters and return values for better understanding of state selection and management.

--- src/core/useSelect.ts
+++ src/core/useSelect.ts
@@ -3,6 +3,19 @@ import useSyncExternalStoreExports from "use-sync-external-store/shim/with-selec
 
 const { useSyncExternalStoreWithSelector } = useSyncExternalStoreExports;
 
+
+/**
+ * A custom hook that subscribes to a store and allows selecting a slice of state, with optional actions, using a selector function.
+ *
+ * @template TState - The type representing the shape of the store's state.
+ * @template TActions - The type representing the actions of the store. Default is `Record<string, unknown>`.
+ * @template TSelected - The type representing the selected slice of state. Default is `TState & TActions`.
+ * 
+ * @param {Store<TState> | StoreWithActions<TState, TActions>} store - The store object to subscribe to, which can either be a basic store or a store with actions.
+ * @param {Selector<TState & TActions, TSelected>} [selector] - A function to select a slice of the store’s state. Defaults to selecting the entire state.
+ * @returns {TSelected} - The selected slice of state.
+ *
+ */
 export const useSelect = <
   TState,
   TActions extends Record<string, ANY> = Record<string, unknown>,
